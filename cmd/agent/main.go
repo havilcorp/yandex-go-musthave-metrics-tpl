f1e8c242e1ef2fc0b9a76a182f67219c627feda5
havilcorp <havilcorp@gmail.com>
2023-12-06 12:48:54 +0300

    test done

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+type Metric struct {
+	gauge   float64
+	counter int64
+}
+
+type MemStorage struct {
+	list map[string]Metric
+}
+
+func main() {
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+	fmt.Println("Alloc", memStats.Alloc)
+	fmt.Println("BuckHashSys", memStats.BuckHashSys)
+	fmt.Println("Frees", memStats.Frees)
+	fmt.Println("GCCPUFraction", memStats.GCCPUFraction)
+	fmt.Println("GCSys", memStats.GCSys)
+	fmt.Println("HeapAlloc", memStats.HeapAlloc)
+	fmt.Println("HeapIdle", memStats.HeapIdle)
+	fmt.Println("HeapInuse", memStats.HeapInuse)
+	fmt.Println("HeapObjects", memStats.HeapObjects)
+	fmt.Println("HeapReleased", memStats.HeapReleased)
+	fmt.Println("HeapSys", memStats.HeapSys)
+	fmt.Println("LastGC", memStats.LastGC)
+	fmt.Println("Lookups", memStats.Lookups)
+	fmt.Println("MCacheInuse", memStats.MCacheInuse)
+	fmt.Println("MCacheSys", memStats.MCacheSys)
+	fmt.Println("MSpanInuse", memStats.MSpanInuse)
+	fmt.Println("MSpanSys", memStats.MSpanSys)
+	fmt.Println("Mallocs", memStats.Mallocs)
+	fmt.Println("NextGC", memStats.NextGC)
+	fmt.Println("NumForcedGC", memStats.NumForcedGC)
+	fmt.Println("NumGC", memStats.NumGC)
+	fmt.Println("OtherSys", memStats.OtherSys)
+	fmt.Println("PauseTotalNs", memStats.PauseTotalNs)
+	fmt.Println("StackInuse", memStats.StackInuse)
+	fmt.Println("StackSys", memStats.StackSys)
+	fmt.Println("Sys", memStats.Sys)
+	fmt.Println("TotalAlloc", memStats.TotalAlloc)
+
+	a := MemStorage{list: map[string]Metric{}}
+	a.list["Alloc"] = Metric{gauge: 1, counter: 2}
+	fmt.Println(a)
+
+	c := time.Tick(2 * time.Second)
+	for next := range c {
+		fmt.Printf("%v\n", next)
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,87 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+type MemStorage struct {
+	listCounter map[string]float64
+	listGauge   map[string]float64
+}
+
+func UpdateCounterHandler(rw http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		rw.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	regex := `^\/update\/counter\/([a-zA-Z0-9]+)\/([a-zA-Z0-9.]+)$`
+	tegexPath := regexp.MustCompile(regex).FindStringSubmatch(r.URL.Path)
+
+	if len(tegexPath) != 3 {
+		rw.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// marketName := tegexPath[1]
+	marketVal := tegexPath[2]
+
+	_, err := strconv.ParseFloat(marketVal, 64)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+
+}
+
+func UpdateGaugeHandler(rw http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		rw.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	regex := `^\/update\/gauge\/([a-zA-Z0-9]+)\/([a-zA-Z0-9.]+)$`
+	tegexPath := regexp.MustCompile(regex).FindStringSubmatch(r.URL.Path)
+
+	if len(tegexPath) != 3 {
+		rw.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// marketName := tegexPath[1]
+	marketVal := tegexPath[2]
+
+	_, err := strconv.ParseFloat(marketVal, 64)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+
+}
+
+func BadTypeHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusBadRequest)
+}
+
+func UpdateOtherHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusNotFound)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/update/counter/", UpdateCounterHandler)
+	mux.HandleFunc("/update/gauge/", UpdateGaugeHandler)
+	mux.HandleFunc("/update/", BadTypeHandler)
+	mux.HandleFunc("/", UpdateOtherHandler)
+
+	http.ListenAndServe(":8080", mux)
+}
